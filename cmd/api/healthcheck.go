cf39eec19e388deaa32b9c8d77f1c029125ca650
p-duke <peteraaronduke@gmail.com>
2025-05-14 21:26:49 -0400

    3.1 create simple json response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,7 +8,12 @@ import (
 // writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON as the HTTP response body
+	w.Write([]byte(js))
 }
+
